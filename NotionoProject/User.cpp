d7b1235f0a2666870bbddd36a5d9177551df5e12
Hristo101 <92890172+Hristo101@users.noreply.github.com>
2026-06-09 15:58:40 +0300

    CreateUserClass

--- NotionoProject/User.cpp
+++ NotionoProject/User.cpp
@@ -1,1 +1,13 @@
 #include "User.h"
+
+User::User(const string& userId, const string& userName, const string& password)
+{
+	if (this->checkUserId.checkUserIdIsInArray(userId))
+	{
+		throw invalid_argument("Invalid userId");
+	}
+
+	this->userId = userId;
+	this->userName = userName;
+	this->password = password;
+}
--- NotionoProject/User.h
+++ NotionoProject/User.h
@@ -1,7 +1,16 @@
 #pragma once
+#include "CheckUserId.h"
+#include <exception>
 class User
 {
-    private:
-
+    protected:
+        CheckUserId checkUserId;
+		string userId;
+		string userName;
+		string password;
+    public:
+		User(const string& userId, const string& userName, const string& password);
+		virtual ~User() = default;
+		virtual void help() const = 0;
 };
 
